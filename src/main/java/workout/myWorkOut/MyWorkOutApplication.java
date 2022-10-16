5aa4d9ebfe66ecd339fad4aabe19cef3d1956052
applzz <hws0602@naver.com>
2022-10-16 14:36:30 +0900

    workOut 프로젝트 생성

---
+++ src/main/java/workout/myWorkOut/MyWorkOutApplication.java
@@ -0,0 +1,13 @@
+package workout.myWorkOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workout/myWorkOut/MyWorkOutApplicationTests.java
@@ -0,0 +1,13 @@
+package workout.myWorkOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
